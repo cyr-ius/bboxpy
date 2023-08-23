32e5ccc1f04197a10f5abf444a3e012a403a7e86
cyr-ius <cyr-ius@ipocus.net>
2023-08-23 16:55:06 +0200

    Publish new api

--- bboxpy/api/__init__.py
+++ bboxpy/api/__init__.py
@@ -2,5 +2,8 @@
 from .device import Device
 from .lan import Lan
 from .wan import Wan
+from .ddns import Ddns
+from .voip import VOIP
+from .iptv import IPTv
 
-__all__ = ["Device", "Lan", "Wan"]
+__all__ = ["Device", "Lan", "Wan", "VOIP", "IPTv", "Ddns"]
