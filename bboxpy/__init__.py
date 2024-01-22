20d093e6d2de196aa62415c2ef3e2bb75b68ac36
cyr-ius <cyr-ius@ipocus.net>
2024-01-22 20:02:18 +0100

    Add exceptions

--- bboxpy/__init__.py
+++ bboxpy/__init__.py
@@ -1,4 +1,18 @@
 """Provides authentication and raw access to Bouygues Bbox."""
 from .bbox import Bbox
+from .exceptions import (
+    AuthorizationError,
+    BboxException,
+    HttpRequestError,
+    ServiceNotFoundError,
+    TimeoutExceededError,
+)
 
-__all__ = ["Bbox"]
+__all__ = [
+    "Bbox",
+    "BboxException",
+    "AuthorizationError",
+    "TimeoutExceededError",
+    "HttpRequestError",
+    "ServiceNotFoundError",
+]
